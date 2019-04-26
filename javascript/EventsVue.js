45372375ac53428a44d14706c8f75996aa9a0e28
haileybrown <hbrown61@uncc.edu>
2019-04-26 01:53:42 -0400

    fixed header

--- javascript/EventsVue.js
+++ javascript/EventsVue.js
@@ -4,30 +4,14 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: "../resources/events/meeting_feb.png",
-        inStock: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
-            }
-        ],
+        image1: "../resources/events/meeting_feb.png",
+        image2: "../resources/events/Poster_Blue.jpg",
+        image3: "../resources/events/Poster_Orange.jpg",
+        image4: "../resources/events/Poster_Pink.jpg",
         cart: 0
     },
     methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateProduct(variantImage) {
-            this.image = variantImage
-        }
+
     }
 })
 
